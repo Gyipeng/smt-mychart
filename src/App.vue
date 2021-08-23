9c666f6c41faa01b4ab3cf9c26ccfc822ba438ba
gyp <826256407@qq.com>
2021-08-23 10:39:06 +0800

    fix:add

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    123
+    <my-chart></my-chart>
+  </div>
+</template>
+
+<script>
+  import myChart from "./components/my-chart.vue"
+export default {
+  name: 'app',
+  components:{myChart},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
